453d048603b3978498569b79065e79ae0b909417
stephanweidinger <ic21b064@technikum-wien.at>
2022-12-10 11:09:43 +0100

    Initial commit

---
+++ src/test/java/com/example/slm_project/SlmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
